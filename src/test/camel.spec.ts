1a4d05c34b7924e83440eabfbc6f40a8041ae531
kakasoo <kscodebase@gmail.com>
2024-03-09 18:19:16 +0900

    feat : add `pascal to camel` type inference

--- src/test/camel.spec.ts
+++ src/test/camel.spec.ts
@@ -7,18 +7,29 @@ import { CamelCase } from '../types/camel';
 describe('camel case test', () => {
     describe('snake to camel', () => {
         it('handles empty string', () => {
-            type A = CamelCase<''>;
+            type Answer = CamelCase<''>;
+            assert.equal(typia.random<Answer>(), '');
             assert.equal(typia.random<ReturnType<typeof camelCase<''>>>(), '');
         });
 
         it('handles single-word string', () => {
-            type A = CamelCase<'hello'>;
+            type Answer = CamelCase<'hello'>;
+            assert.equal(typia.random<Answer>(), 'hello');
             assert.equal(typia.random<ReturnType<typeof camelCase<'hello'>>>(), 'hello');
         });
 
         it('handles string with single underscore', () => {
-            type A = CamelCase<'hello_world'>;
+            type Answer = CamelCase<'hello_world'>;
+            assert.equal(typia.random<Answer>(), 'helloWorld');
             assert.equal(typia.random<ReturnType<typeof camelCase<'hello_world'>>>(), 'helloWorld');
+        });
+    });
+
+    describe('pascal to camel', () => {
+        it('converts Pascal case to camel case', () => {
+            type Answer = CamelCase<'ThisIsAnExample'>;
+            assert.equal(typia.random<Answer>(), 'thisIsAnExample');
+            assert.equal(typia.random<ReturnType<typeof camelCase<'ThisIsAnExample'>>>(), 'thisIsAnExample');
         });
     });
 });
--- src/types/camel.ts
+++ src/types/camel.ts
@@ -6,4 +6,4 @@ import { IsSnakeCase } from './snake';
 export type SnakeToCamel<SnakeCase extends string> = Uncapitalize<StringArrayToPascalCase<Split<SnakeCase, '_'>>>;
 
 export type CamelCase<T extends string> =
-    IsStringLiteral<T> extends true ? (IsSnakeCase<T> extends true ? SnakeToCamel<T> : T) : string;
+    IsStringLiteral<T> extends true ? (IsSnakeCase<T> extends true ? SnakeToCamel<T> : Uncapitalize<T>) : string;
