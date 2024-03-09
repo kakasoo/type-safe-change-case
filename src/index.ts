02526a020af7a87f05dd047ff2d8f9d126983d3d
kakasoo <kscodebase@gmail.com>
2024-03-09 17:01:10 +0900

    feat : first commit ( wrapping the `change-case` library )

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import changeCase from "change-case";
+
+export function camelCase<T extends string>(input: T) {
+    return changeCase.camelCase(input);
+}
+
+export function capitalCase<T extends string>(input: T) {
+    return changeCase.capitalCase(input);
+}
+
+export function constantCase<T extends string>(input: T) {
+    return changeCase.constantCase(input);
+}
+
+export function dotCase<T extends string>(input: T) {
+    return changeCase.dotCase(input);
+}
+
+export function kebabCase<T extends string>(input: T) {
+    return changeCase.kebabCase(input);
+}
+
+export function noCase<T extends string>(input: T) {
+    return changeCase.noCase(input);
+}
+
+export function pascalCase<T extends string>(input: T) {
+    return changeCase.pascalCase(input);
+}
+
+export function pascalSnakeCase<T extends string>(input: T) {
+    return changeCase.pascalSnakeCase(input);
+}
+
+export function pathCase<T extends string>(input: T) {
+    return changeCase.pathCase(input);
+}
+
+export function sentenceCase<T extends string>(input: T) {
+    return changeCase.sentenceCase(input);
+}
+
+export function snakeCase<T extends string>(input: T) {
+    return changeCase.snakeCase(input);
+}
+
+export function trainCase<T extends string>(input: T) {
+    return changeCase.trainCase(input);
+}
