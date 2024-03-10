68cb35bcf2f8c733903fb3220ccc6aa5e2a863f8
kakasoo <kscodebase@gmail.com>
2024-03-10 19:56:38 +0900

    fix : CONSTANT_CASE

--- src/types/camel.ts
+++ src/types/camel.ts
@@ -2,14 +2,18 @@ import { StringType } from '@kakasoo/proto-typescript';
 import { IsStringLiteral } from './common';
 import { StringArrayToPascalCase } from './pascal';
 
+type __Camelize<T extends string> = Uncapitalize<
+    StringType.Includes<T, '_' | ' ' | '.' | '-' | '/'> extends true
+        ? StringArrayToPascalCase<StringType.SplitByManySplitter<[T], ['_', ' ', '.', '-', '/']>>
+        : Uncapitalize<T>
+>;
+
 export type CamelCase<T extends string> = T extends ''
     ? ''
     : IsStringLiteral<T> extends true
-      ? Uncapitalize<
-            StringType.Includes<T, '_' | ' ' | '.' | '-' | '/'> extends true
-                ? StringArrayToPascalCase<StringType.SplitByManySplitter<[T], ['_', ' ', '.', '-', '/']>>
-                : Uncapitalize<T>
-        >
+      ? T extends Uppercase<T>
+          ? __Camelize<Lowercase<T>>
+          : __Camelize<T>
       : string;
 
 export type IsCamelCase<T extends string> = CamelCase<T> extends T ? true : false;
